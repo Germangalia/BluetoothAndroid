9d2574a37ef270997655c2423232f2a45947db97
Germangalia <germangalia@iesebre.com>
2016-02-16 19:22:50 +0100

    Implement method onActivityResult.

--- app/src/main/java/com/example/ggalia84/bluetoothtinkering/MainActivity.java
+++ app/src/main/java/com/example/ggalia84/bluetoothtinkering/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.ggalia84.bluetoothtinkering;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
+    private static final String TAG = "BT MainActivity";
     private BluetoothAdapter mBluetoothAdapter;
 
     @Override
@@ -40,7 +43,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        
+        switch (requestCode){
+
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    Log.d(TAG, "BT not enabled");
+                    Toast.makeText(this, "Bluetooth no s'ha creat.",
+                            Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+        }
     }
 
     @Override
